23d08e8276f12dd20e7d5b9058c20f366a8b9969
samuelgolovin <93148633+samuelgolovin@users.noreply.github.com>
2024-12-02 15:10:29 -0600

    Delete sliding_window_practice.py

--- sliding_window_practice.py
+++
@@ -1,9 +0,0 @@
-def max_sum_subarray(arr, k):
-    max_sum = sum(arr[:k])
-    window_sum = max_sum
-
-    for i in range(len(arr) - k):
-        window_sum = window_sum - arr[i] + arr[i + k]
-        max_sum = max(max_sum, window_sum)
-
-    return max_sum
