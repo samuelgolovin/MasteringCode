c892853d25d3550a0a6d572cacd65497e5085797
samuelgolovin <93148633+samuelgolovin@users.noreply.github.com>
2024-12-03 13:54:24 -0600

    Update sliding_window_practice.py

--- week1/sliding_window_practice.py
+++ week1/sliding_window_practice.py
@@ -7,3 +7,6 @@ def max_sum_subarray(arr, k):
         max_sum = max(max_sum, window_sum)
 
     return max_sum
+    
+my_array = [1, 2, 3, 4, 5, 2, 1, 7, 1]
+print(max_sum_subarray(my_array, 2)) # output 9 because the largest combination of 2 numbers right next to each other is 4 and 5
