2d881520b8acffd9405c49ced7004811036d5f3d
samuelgolovin <93148633+samuelgolovin@users.noreply.github.com>
2024-12-10 12:51:12 -0600

    Create QueueUsingStacks_practice.py

---
+++ week2/practice/QueueUsingStacks_practice.py
@@ -0,0 +1,22 @@
+class QueueUsingStacks:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, x):
+        self.stack1.append(x)
+
+    def dequeue(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        if self.stack2:
+            return self.stack2.pop()
+        else:
+            return None
+
+queue = QueueUsingStacks()
+queue.enqueue(1)
+queue.enqueue(2)
+print(queue.dequeue())  # Output: 1
+print(queue.dequeue())  # Output: 2
